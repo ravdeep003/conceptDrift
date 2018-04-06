c358d06a38057c3da52d23e171ab9e99e60800b9
Ravdeep <rpasr001@ucr.edu>
2018-04-05 22:03:15 -0700

    changed generator script

--- createDatasetGeneric.m
+++ createDatasetGeneric.m
@@ -1,4 +1,4 @@
-function [A, B, C,initialRank, X] = createDatasetGeneric(I, J, K, R,batch)
+function [A, B, C,initialRank] = createDatasetGeneric(I, J, K, R,batch)
 % I,J & K should be multiple of 100
 % It will output factor matrices and a tensor of full Rank R
 A = rand(I, R);
@@ -9,7 +9,8 @@ last = batch;
 temp = generateC(R, batch);
 
 iter = K/batch;
-n = randperm(size(temp,1)/batch, iter);
+% n = randperm(size(temp,1)/batch, iter);
+n = randi([1,size(temp,1)/batch],1,iter);
 n(iter) = size(temp,1)/batch;
 for i=1:iter
     tempIn =  (n(i)-1)*batch + 1;
@@ -19,6 +20,6 @@ for i=1:iter
     initial = initial + batch;
     last = last + batch;
 end
-X = tensor(ktensor(ones(R,1),A, B ,C ));
+% X = tensor(ktensor(ones(R,1),A, B ,C ));
 initialRank = size(find(sum(C(1:batch,:)>0)),2);
 end
